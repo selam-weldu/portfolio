1de5a9877268d6278f9f8d30d2c3e84cd55b3e75
Selam Weldu <ssweldu@gmail.com>
2019-07-30 22:46:21 -0700

    add modal.js

---
+++ assets/js/modal.js
@@ -0,0 +1,27 @@
+var modal = document.querySelectorAll(".modal");
+var trigger = document.querySelectorAll(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal(id) {
+    var modal = document.querySelector(".modal" + "." + id)
+    modal.classList.toggle("show-modal");
+}
+
+function handleTrigger() {
+    let id = event.currentTarget.classList[2];
+    toggleModal(id);
+}
+
+function windowOnClick(event) {
+    if (event.target.parentElement.parentElement.classList[0] === modal[1].classList[0]) {
+        toggleModal(event.target.parentElement.parentElement.classList[1]);
+    } else if (event.target.classList[0] == modal[1].classList[0]) {
+        toggleModal(event.target.classList[1]);
+    }
+}
+
+trigger.forEach((el) => {
+    el.addEventListener("click", handleTrigger);
+});
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
---
+++ assets/js/slide.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showDivs(slideIndex, 'one');
+showDivs(slideIndex, 'two');
+// showDivs(slideIndex, 'three');
+// showDivs(slideIndex, 'four');
+
+
+function plusDivs(n, idx) {
+    showDivs(slideIndex += n, idx);
+}
+
+function showDivs(n, idx) {
+    var i;
+    var x = document.getElementsByClassName("mySlides " + idx);
+    if (x.length != 0) {
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length };
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+}
